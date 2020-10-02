0a8185381e0889f40dd8e0486d10d13efb53e607
MartimRosado <martim.rosado@outlook.com>
2020-10-02 20:22:06 +0100

    Update from iob-soc

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -2,12 +2,27 @@
 #include "system.h"
 #include "periphs.h"
 #include "iob-uart.h"
+#include "iob_timer.h"
 
 int main()
 {
+  unsigned long long elapsed;
+  unsigned int elapsedu;
+
+  //read current timer count, compute elapsed time
+  elapsed = timer_get_count(TIMER_BASE);
+  elapsedu = timer_time_us(TIMER_BASE);
+
   //init uart 
   uart_init(UART_BASE,FREQ/BAUD);   
   uart_printf("\n\n\nHello world!\n\n\n");
   //char *a = malloc(10);
   //free(a);
+
+  uart_txwait();
+
+  uart_printf("\nExectution time: %d clocks in %dus @%dMHz\n\n", (unsigned int)elapsed, elapsedu, FREQ/1000000);
+
+  uart_txwait();
+  return 0;
 }
